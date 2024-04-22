0b4d397e087916e11d80001ca00b91cb43002d13
ssb3vk <ssb3vk@virginia.edu>
2024-04-22 12:47:37 -0400

    added print statements for term and trunc

--- catanatron_gym/catan_med_DQN.py
+++ catanatron_gym/catan_med_DQN.py
@@ -409,8 +409,10 @@ def train_models(algorithm):
                 if algorithm == "DQN" or "DDQN" or "DQ3N" or "DDQN3D":
                     state.to(device)
                     # print("iwthin decision")
-                    if state.dim() == 4:  # Check if the batch dimension is missing
+                    while state.dim() <= 4:  # Check if the batch dimension is missing
                         state = state.unsqueeze(0)  # Add batch dimension
+                    print(terminated)
+                    print(truncated)
                     print(state.dim())
 
                     # select a_t = argmax_a Q(s_t, a; theta)
